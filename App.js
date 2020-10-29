6f84bc1fa57a9d4fc02a1b2802b199fff73b20c0
vikas <bossvicky42@gmail.com>
2020-10-29 16:37:03 +0530

    facebook setup completed

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ const App = () => {
 
   return <NavigationContainer>
     <Stack.Navigator
-      initialRouteName='HomeScreen'
+      initialRouteName='LoginScreen'
       screenOptions={{
         headerStyle: { backgroundColor: colorPrimary },
         headerTintColor: colorWhite,

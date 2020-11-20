5cd4b057da872db8084f783c2894a43f14453260
vikas <bossvicky42@gmail.com>
2020-11-20 15:28:14 +0530

    image picker functionality implemented

--- src/helpers/Utils.js
+++ src/helpers/Utils.js
@@ -1,4 +1,5 @@
 import { Linking } from "react-native"
+import { colorAccent } from "../theme/Color";
 
 export const openLink = (navigation, title, url) => {
     navigation.push('WebViewScreen', {
@@ -18,3 +19,21 @@ export const PASSWORD_ERROR_MESSAGE = 'Please enter atleast 4 digit password.';
 export const validatePass = pass => {
     return pass !== undefined && pass.length >= 4;
 }
+export const DEFAULT_PICKER_OPTIONS = {
+    mediaType: 'photo',
+    quality: 0,
+    allowsEditing: true,
+    tintColor: colorAccent,
+    storageOptions: { privateDirectory: true }
+};
+
+// export const DEFAULT_PICKER_OPTIONS = () => {
+//     return {
+//         mediaType: 'photo',
+//         quality: 0,
+//         allowsEditing: true,
+//         tintColor: colorAccent,
+//         storageOptions: { privateDirectory: true }
+//     }
+// };
+

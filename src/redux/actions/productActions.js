525e4ce3926e8b3b3efa6f9b2a36d8f3271c3314
vikas <bossvicky42@gmail.com>
2020-12-01 11:52:24 +0530

    Home Page Finishing work

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -2,7 +2,7 @@
 import Api from '../../services/api';
 import Store from '../Store';
 import { log } from '../../components/Logger';
-import { PRODUCT_LIST_SUCCESS, PRODUCT_LIST_REQUEST, PRODUCT_LIST_FAIL } from '../../constants';
+import { PRODUCT_LIST_SUCCESS, PRODUCT_LIST_REQUEST, PRODUCT_LIST_FAIL, UPDATE_PRODUCT_REQUEST, UPDATE_PRODUCT_FAIL } from '../../constants';
 
 export const getProductList = () => {
     return (dispatch) => {
@@ -18,6 +18,48 @@ export const getProductList = () => {
             .catch(err => {
                 dispatch({ type: PRODUCT_LIST_FAIL });
                 log('Error in Product Listing:', err);
+            })
+    }
+}
+
+export const getProductById = (id) => {
+    return (dispatch) => {
+        return Api.get(`/product/getProductById/${id}`)
+            .then(response => {
+
+            })
+            .catch(err => {
+                log('Error Retriving Product', err);
+            })
+    }
+}
+
+export const getProductByBarcode = (barcode) => {
+    return (dispatch) => {
+        return Api.get(`/product/getProductByBarcode/${barcode}`)
+            .then(response => {
+
+            })
+            .catch(err => {
+                log('Error Retriving Product From barcode:', err);
+            })
+    }
+}
+
+export const createProduct = (navigation, body) => {
+    return (dispatch) => {
+        dispatch({ type: UPDATE_PRODUCT_REQUEST });
+        const { authData } = Store.getState().auth;
+        return Promise.all([
+            Api.get(`/product/checkexistingitem/${authData.id}/${body.icode}`),   //Checking Product already exist with same icode
+            Api.get(`/product/checkexistingbarcode/${body.barcode}`)              //Check for existing barcode
+        ])
+            .then(response => {
+                //hit api to create product
+            })
+            .catch(err => {
+                log('Error Checking for Product Creation', err);
+                dispatch({ type: UPDATE_PRODUCT_FAIL });
             })
     }
 }

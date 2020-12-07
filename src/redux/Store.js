779943e21c837823569d84f4aa45eb7b4c248ae4
vikas <bossvicky42@gmail.com>
2020-12-07 18:53:59 +0530

    employees api setup

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -3,6 +3,7 @@ import invoiceReducer from "./reducer/invoiceReducer";
 import productReducer from "./reducer/productReducer";
 import contactReducer from "./reducer/contactReducer";
 import ledgerReducer from "./reducer/ledgerReducer";
+import userReducer from "./reducer/userReducer";
 
 const { combineReducers, createStore, applyMiddleware } = require("redux");
 const { default: thunk } = require("redux-thunk");
@@ -13,7 +14,8 @@ const appReducers = combineReducers({
     invoice: invoiceReducer,
     product: productReducer,
     contact: contactReducer,
-    ledger: ledgerReducer
+    ledger: ledgerReducer,
+    user: userReducer
 });
 
 const Store = createStore(appReducers, applyMiddleware(thunk));
